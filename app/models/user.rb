13f1c0af7f1a62debc09dd27d91b4ebaa80d5e7d
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-12-18 18:45:42 +0200

    fixed username not showing

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments
 
+  validates :username, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
